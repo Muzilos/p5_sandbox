b4f2ec606deb7e28d7679e598b793dd5890a42ac
muzilos <smm321@gmail.com>
2020-10-11 21:33:53 -0400

    implement recording

--- circles/sketch.js
+++ circles/sketch.js
@@ -6,10 +6,13 @@
 let x, y;
 var h_center
 var w, h
-let fr = 600
+let fr = 300
+
+const btn = document.querySelector('button'),
+chunks = [];
 
 function setup() {
-  createCanvas(500, 400);
+  createCanvas(1280, 1080);
   w = width
   h = height
   w_center = w / 2
@@ -22,15 +25,15 @@ function setup() {
 }
 
 function draw() {
-  store()
-  door()
+  store(20, 40)
+  door(10, 10)
 }
 
-function store() {
+function store(size_x, size_y) {
   // Draw a rect
   stroke(100);
   fill(100);
-  rect(x, y, 24, 24);
+  rect(x, y, size_x, size_y);
   
   // Jiggling randomly on the horizontal axis
   x = w_center + random(-w/5, w/5);
@@ -43,13 +46,13 @@ function store() {
   }
 }
 
-function door() {
+function door(size_x, size_y) {
     let mod_x = 0.175
     let mod_y = 0.4
     // Draw a rect
     stroke(0);
     fill('#faf');
-    rect((x * mod_x), (y * mod_y), 5, 5);
+    rect((x * mod_x), (y * mod_y), size_x, size_y);
     
     // Jiggling randomly on the horizontal axis
     x = w_center + random(-(w * mod_x)/5, (w * mod_x)/5);
@@ -61,3 +64,34 @@ function door() {
       y = (h_center * mod_y);
     }  
 }
+
+
+function record() {
+  chunks.length = 0;
+  let stream = document.querySelector('canvas').captureStream(30),
+    recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = e => {
+    if (e.data.size) {
+      chunks.push(e.data);
+    }
+  };
+  recorder.onstop = exportVideo;
+  btn.onclick = e => {
+    recorder.stop();
+    btn.textContent = 'start recording';
+    btn.onclick = record;
+  };
+  recorder.start();
+  btn.textContent = 'stop recording';
+}
+
+function exportVideo(e) {
+  var blob = new Blob(chunks);
+  var vid = document.createElement('video');
+  vid.id = 'recorded'
+  vid.controls = true;
+  vid.src = URL.createObjectURL(blob);
+  document.body.appendChild(vid);
+  vid.play();
+}
+btn.onclick = record;
